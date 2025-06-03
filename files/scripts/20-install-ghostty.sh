4a6695fbc8a9428f419dd350ef2da867af8b84d2
CRBroughton <crbroughton@posteo.uk>
2025-06-03 12:25:57 +0100

    replace ghostty script with recipe.yml copr repository link

--- files/scripts/20-install-ghostty.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-set -oue pipefail
-
-dnf copr enable pgdev/ghostty
-dnf install ghostty
